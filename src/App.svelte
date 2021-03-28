12817ec39a3b7e50d5df95eb465040f6f268509c
irisida <20445648+irisida@users.noreply.github.com>
2021-03-28 16:31:21 +0100

    added be ref file, added ViewAdd component

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,11 @@
 <script>
   import Obj from './components/Obj.svelte';
+  import List from './components/List.svelte';
 
   import Renderer from './components/Renderer.svelte';
+  import ViewAdd from './components/ViewAdd.svelte';
+
+  const date = Date.now() / 1000;
 
   const content = `# markdown here!
   Here's a **markdown** based post.`;
@@ -10,3 +14,7 @@
 <Obj objectId="00000000-0000-0000-0000-00000000" />
 
 <Renderer title="hello" {content} />
+
+<List {date} />
+
+<ViewAdd />
