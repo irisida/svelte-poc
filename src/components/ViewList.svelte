a8fd3dd48139b5939fb507846e7b286f84be9f70
irisida <20445648+irisida@users.noreply.github.com>
2021-03-28 17:41:49 +0100

    added fade transitions to views

--- src/components/ViewList.svelte
+++ src/components/ViewList.svelte
@@ -1,8 +1,12 @@
 <script>
   import Calendar from './Calendar.svelte';
   import List from './List.svelte';
+  import { fade } from 'svelte/transition';
+
   let date = null;
 </script>
 
-<Calendar bind:date />
-<List bind:date />
+<div in:fade>
+  <Calendar bind:date />
+  <List bind:date />
+</div>
