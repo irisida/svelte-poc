ccb66ba4a5bbc111d494dda8ebf98977e606f3fb
irisida <20445648+irisida@users.noreply.github.com>
2021-03-28 13:41:59 +0100

    created base level poc for journaling app. Uses BE in a docker container

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Renderer from './components/Renderer.svelte';
+</script>
+
+<!-- <script>
+  export let name = '';
+</script>
+
+<h1>Hello, {name}!</h1>
+<p>A first Svelte App</p>
+
+<style>
+  p {
+    color: #1d4585;
+  }
+</style> -->
+
+<Renderer title="hello" content="World" />
